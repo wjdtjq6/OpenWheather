e17f1dc37e3bff61ae025d10f979e7771b5bbe6f
jimmy <wjdtjq6@gmail.com>
2024-06-19 15:31:57 +0900

    Initial Commit

---
+++ OpenWheather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWheather
+//
+//  Created by t2023-m0032 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
